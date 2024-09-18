a39ee789a073582fe063f416576ea8764c225916
Shawn-hash <77256317+Shawn-hash@users.noreply.github.com>
2024-09-18 16:11:03 +0800

    Update Person.cs

--- PersonEntity/Models/Person.cs
+++ PersonEntity/Models/Person.cs
@@ -1,4 +1,4 @@
-﻿namespace PersonEntity.Models
+namespace PersonEntity.Models
 {
     public class Person
     {
@@ -7,5 +7,18 @@
         public string LastName { get; set; }
         public int Age { get; set; }
         public DateTime LastModifiedOn { get; set; }
+
+        // Constructor to initialize properties
+        public Person(string firstName, string lastName, int age)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            LastModifiedOn = DateTime.UtcNow;
+        }
+
+        // Parameterless constructor for model binding
+        public Person() { }
     }
 }
